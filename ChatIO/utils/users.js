85aa331aa55b8ec1a514cc50e86b6d07b9ba9eaa
Diego Alonso Rubí Salas <43016665+Diegoalru@users.noreply.github.com>
2024-07-18 17:08:56 +0000

    Refactor users.js to use module syntax.

--- ChatIO/utils/users.js
+++ ChatIO/utils/users.js
@@ -4,65 +4,43 @@ const usersOnline = [];
 
 //Ingresar un usuario a la sala.
 function userJoin(id, username, room) {
-
-    //Crea el objeto que contiene todos los valores de la sesión.
     const user = { id, username, room };
-
-    //Ingresa el usuario al la lista.
     users.push(user);
-
-    //Retorna el usuario creado
     return user;
 }
 
-//Obtiene los valores del usuario actual
 function getCurrentUser(id) {
-
-    //Se retorna los valores del suaurio según su Id
     return users.find(user => user.id === id);
 }
 
-//Elmina la sesion de un usuario
 function userLeaves(id) {
-
-    //Se realiza la consulta del usuario en la lista
     const userId = users.findIndex(user => user.id === id);
 
-    //
     if (userId !== -1) {
         return users.splice(userId, 1)[0];
     }
 }
 
-//Obtener usuarios de una sala.
 function getRoomUsers(room) {
-
-    //Retorna los usuarios que existan en la sala seleccionada.
     return users.filter(user => user.room === room);
 }
 
 function Register(username, email, password, passwordConfirmation) {
-    if (ValidatePasswords(password, passwordConfirmation)) {
-        if (!ExisteEmail(email)) {
-            if (!ExisteUsername(username)) {
-                const user = { username, email, password };
-                users.push(user);
-                return 0;
-            } else {
-                return -3;
-            }
-        } else {
-            return -2;
-        }
-    } else {
+    if (password !== passwordConfirmation) {
         return -1;
     }
-}
 
-function ValidatePasswords(psw1, psw2) {
-    if (psw1 !== psw2)
-        return false;
-    return true;
+    if (ExisteEmail(email)) {
+        return -2;
+    }
+
+    if (ExisteUsername(username)) {
+        return -3;
+    }
+
+    const user = { username, email, password };
+    users.push(user);
+    return 0;
 }
 
 function ExisteEmail(email) {
@@ -115,12 +93,11 @@ function DeleteSession(socket) {
     }
 }
 
-
-module.exports = {
-    Register
-    , Login
-    , userJoin
-    , getCurrentUser
-    , userLeaves
-    , getRoomUsers
+export {
+    Register,
+    Login,
+    userJoin,
+    getCurrentUser,
+    userLeaves,
+    getRoomUsers
 };
